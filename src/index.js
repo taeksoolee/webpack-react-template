36ca93024a2457ad03ea1d9843f8d281114bf176
leetaeksoo <leets1490@gmail.com>
2022-04-06 09:57:56 +0900

    fix in the React18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// before 18
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// after 18
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
