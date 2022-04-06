c15c6093296c137aeb984e6b042a6fe34b82ee67
leetaeksoo <leets1490@gmail.com>
2022-04-06 12:18:44 +0900

    add server src

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+
+function sleep(ms) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {res(true)}, ms);
+  })
+}
+
+app.get('/', async function(req, res) {
+  await sleep(5000);
+  res.json({'hello' : 'world'});  
+});
+
+require('http').createServer(app).listen(7777, () => {
+  console.log('listen server');
+});
