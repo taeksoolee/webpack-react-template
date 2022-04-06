eedbdbadaf1dba203b1286894b0803e102ef06e6
leetaeksoo <leets1490@gmail.com>
2022-04-06 11:17:24 +0900

    add file-loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import css from './App.css'
 import Counter from './components/Counter';
 
+import clouds from './images/clouds-g49df5b05e_1920.png';
+
 const App = () => (
   <>
     <div className="title">
@@ -9,6 +11,8 @@ const App = () => (
     </div>
     <hr />
     <Counter />
+    <hr />
+    <img src={clouds} />
   </>
 );
 export default App;
