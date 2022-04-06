00de3a09159f014f1ba27aa0011bff542bcbcebb
leetaeksoo <leets1490@gmail.com>
2022-04-06 10:21:29 +0900

    setting favicon.ico

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
+      hash: true,
+      favicon: "favicon/favicon.ico",
     })
   ],
   resolve: {
